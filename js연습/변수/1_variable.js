da1b686fc37557576831789111be116000d0adf3
sklee2685 <sklee2685@gmail.com>
2022-09-22 16:59:29 +0900

    code

---
+++ js연습/변수/1_variable.js
@@ -0,0 +1,11 @@
+let a = 0; // 변수 a에 0이라는 값을 할당
+console.log(a); //console.log를 통해 변수 a 값인 0이 출력됨
+
+a = 1; // 변수 a에있던 0대신 1를 재할당
+console.log(a); //1을 재할당 했으므로 1이 출력됨
+
+let b //아무것도 정의되어있지 X
+console.log(b); //아무것도 정의 되어 있지 않으므로 undefined가 출력됨
+
+b = 2; //아무것도 정의 되어있지 않은 변수 b에 2라는 값을 재할당
+console.log(b); //결과:2가 출력됨
---
+++ js연습/변수/2_naming.js
@@ -0,0 +1,13 @@
+//나쁜 예제
+let number = 20; //의미가 없음
+
+//좋은예제
+let myAge = 20; //내 나이라는 의미가 있음
+
+//나쁜예제
+let audio1;
+let audio2;
+
+//좋은예제 (조금더 구체적으로)
+let backgroundAudio;
+let windAudio;

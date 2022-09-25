fabede11f78a28915dbdf6ba7924077cd929df94
sklee2685 <sklee2685@gmail.com>
2022-09-25 21:56:56 +0900

    code

---
+++ js연습/연산자/2_arithmetic.js
@@ -0,0 +1,24 @@
+// 산술 연산자(Arithmetic operators)
+// + 더하기
+console.log("5 + 2 = " + (5+2))
+
+// - 빼기
+console.log("5 - 2 = " + (5-2))
+
+// * 곱하기
+console.log("5 * 2 = " + (5*2))
+
+// / 나누기
+console.log("4 / 2 = " + (4/2))
+
+// % 나머지 값
+console.log("5 % 2 = " + (5%2))
+
+// ** 지수 (거듭제곱)
+console.log("5 ** 2 = " + (5**2))
+
+// +연산자 주의점!
+let text = '두개의' + '문자를 연결';
+console.log(text);
+text = '1' + 1; // 숫자와 문자열을 더하면 문자열로 변환됨
+console.log(text);

ccfefbc05f84c81ef740deaf8a478b62133a9e29
sklee2685 <sklee2685@gmail.com>
2022-09-25 20:20:35 +0900

    code

---
+++ js연습/변수/10_typeof.js
@@ -0,0 +1,16 @@
+// typeof: 데이터 타입을 확인
+// 값을 타입 문자열로 변환해줌
+
+let variable;
+console.log(typeof variable);
+
+variable = '';
+console.log(typeof variable);
+
+variable = 123;
+console.log(typeof variable);
+
+variable = {};
+console.log(typeof variable);
+
+// 자바스크립트는 동적으로 실행될때 할당된 값에 따라 타입이 결정됨

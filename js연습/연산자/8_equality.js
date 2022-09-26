63169b17797d0e6a6bcbca40f2dd27a5f580e4ba
sklee2685 <sklee2685@gmail.com>
2022-09-26 22:54:19 +0900

    code

--- js연습/연산자/8_equality.js
+++ js연습/연산자/8_equality.js
@@ -22,11 +22,11 @@ const obj2 = {
   name: 'js',
 };
 
-console.log(obj1 == obj2);
-console.log(obj1 === obj2);
-console.log(obj1.name == obj2.name);
-console.log(obj1.name === obj2.name);
+console.log(obj1 == obj2); // false object에는 값이 아닌 메모리 주소가 저장되어있어 false가 출력됨
+console.log(obj1 === obj2);// 값자체가 다르므로 타입이 동일해도 false 가 출력됨
+console.log(obj1.name == obj2.name); //이건 주소가 아닌 값을 비교하는 것 이므로 true 
+console.log(obj1.name === obj2.name);// true
 
 let obj3 = obj2;
-console.log(obj3 == obj2);
-console.log(obj3 === obj2);
+console.log(obj3 == obj2); //동일한 주소값을 가지고 있으로 true
+console.log(obj3 === obj2); // 값과 타입 둘다 동일하므로 true

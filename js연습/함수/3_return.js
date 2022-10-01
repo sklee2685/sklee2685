cc5cf1ac12e529095e32e60f3d9240f70aace286
sklee2685 <sklee2685@gmail.com>
2022-10-01 19:31:38 +0900

    code

--- js연습/함수/3_return.js
+++ js연습/함수/3_return.js
@@ -6,7 +6,7 @@ const result = add(1,2);
 console.log(result); // undefined 출력
 
 // return을 함수 중간에 하게 되면 함수가 종료됨
-// 사용예: 조건이 맞지 않는 경우 함수 도입부분에서 함수를 일찍 종료함!
+// 사용예) 조건이 맞지 않는 경우 함수 도입부분에서 함수를 일찍 종료함
 function print(num){
     if(num < 0){
         return; // return으로 인해 num의 숫자가 0보다 작으면 프로그램이 종료됨

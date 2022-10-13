b41bd4dc4e0ff0bb64889ee0245f60811bf2f8f3
sklee2685 <sklee2685@gmail.com>
2022-10-13 23:17:24 +0900

    code

---
+++ 3_boolean.js
@@ -0,0 +1,24 @@
+// 블리언 object
+
+const isTrue = true; // new Boolean(true);
+console.log(isTrue.valueOf());
+
+/**
+ * false를 출력하는 값
+ * falshy
+ * 0
+ * -0
+ * null
+ * NaN
+ * undefined
+ */
+
+/**
+ * true를 출력하는 값
+ * 1
+ * -1
+ * '0'
+ * 'false'
+ * []
+ * {}
+*/
